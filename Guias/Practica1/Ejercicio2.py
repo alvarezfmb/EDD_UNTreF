13f0aed30c74ab61d4b3dcedfad4b3acb23e62f9
Felipe Álvarez <alvarezfmb@gmail.com>
2016-08-12 19:40:18 -0300

    Initial commit

--- Guias/Practica1/Ejercicio2.py
+++ Guias/Practica1/Ejercicio2.py
@@ -1,7 +1,7 @@
 # extraer palabra de un texto y guardarla en variable
 
 def extractor(texto,palabraAExtraer):
-    longitud = texto.length
+    longitud = len(texto)
     posicion = texto.find(palabraAExtraer)
     if posicion < 0:
         return "No se encontro la palabra"
