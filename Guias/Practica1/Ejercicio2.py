bb3de581e82e43306c7ae78274a20399de7bf06e
Felipe Álvarez <alvarezfmb@gmail.com>
2016-08-12 19:10:51 -0300

    Initial commit

---
+++ Guias/Practica1/Ejercicio2.py
@@ -0,0 +1,17 @@
+# extraer palabra de un texto y guardarla en variable
+
+def extractor(texto,palabraAExtraer):
+    longitud = texto.length
+    posicion = texto.find(palabraAExtraer)
+    if posicion < 0:
+        return "No se encontro la palabra"
+    else:
+        extraccion = texto[posicion:(posicion + longitud)]
+        return extraccion
+
+
+t1 = "En Estructura de Datos usamos Python como lenguaje de programacion"
+p1 = "usamos"
+
+print(extractor(t1,p1))
+print(p1)
